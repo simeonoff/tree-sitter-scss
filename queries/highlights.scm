f4e26372cfc4f7ff596324c25cc5f79a1c77400a
Simeon Simeonoff <hello@simeonoff.ninja>
2026-02-24 07:21:46 +0200

    feat: add grammar and highlights for `<supports-feature>` syntax (#8)
    * feat: add grammar and highlights for `<supports-feature>` syntax
    * fix(highlight): scope bare keyword highlights to parent nodes to prevent false matches

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -9,6 +9,7 @@
   (universal_selector)
   (nesting_selector)
 ] @tag
+
 (attribute_selector (plain_value) @string)
 (parenthesized_query
   (keyword_query) @property)
@@ -16,11 +17,14 @@
 [
   "~"
   ">"
+  "<"
   "+"
   "-"
   "*"
   "/"
   "="
+  "=="
+  "!="
   "^="
   "|"
   "|="
@@ -32,37 +36,58 @@
   (range_operator)
 ] @operator
 
+; Scope keyword operators to their parent nodes to avoid
+; false matches inside identifiers (e.g. "not" in "annotation")
 [
-  "and"
-  "or"
-  "not"
-  "only"
+  (binary_query "and")
+  (binary_query "or")
+  (unary_query "not")
+  (unary_query "only")
+  (style_query "and")
+  (style_query "or")
+  (style_query "not")
+  (scroll_state_query "and")
+  (scroll_state_query "or")
+  (scroll_state_query "not")
+  (if_style_condition "and")
+  (if_style_condition "or")
+  (if_style_condition "not")
+  (binary_expression "and")
+  (binary_expression "or")
+  (unary_expression "not")
 ] @keyword.operator
 
-(attribute_selector (plain_value) @string)
 (pseudo_element_selector "::" (tag_name) @selector.pseudo)
 (pseudo_class_selector ":" (class_name) @selector.pseudo)
+(page_pseudo_class) @selector.pseudo
 
-(variable_name) @variable.other.member
-(variable_value) @variable.other.member
+[
+  (variable_name)
+  (variable_value)
+  (container_statement (container_name))
+] @variable.other.member
+
 (argument_name) @variable.parameter
 
 [
   (feature_name)
   (identifier)
+  (property_name)
 ] @property
-
-((property_name) @property)
 
 (id_name) @selector.id
 (class_name) @selector.class
+(placeholder_name) @selector.class
 (namespace_name) @namespace
 (namespace_selector (tag_name) @namespace "|")
+(variable_module (module) @namespace)
 
 (attribute_name) @attribute
 
-(function_name) @function
-(mixin_name) @function
+[
+  (function_name)
+  (mixin_name)
+] @function
 
 (function_statement (name) @function)
 (mixin_statement (name) @function)
@@ -71,14 +96,13 @@
   (plain_value)
   (keyframes_name)
   (keyword_query)
+  (feature_value)
 ] @constant.builtin
 
 (interpolation "#{" @punctuation.special "}" @punctuation.special)
-(property_name (interpolation (variable_value) @variable.other.member))
 
 [
   "@media"
-  "@import"
   "@charset"
   "@namespace"
   "@supports"
@@ -105,24 +129,38 @@
   (to)
   (from)
   (important)
+  (default)
+  (global)
+  (margin_at_keyword)
+  (font_feature_value_keyword)
 ] @keyword
 
-(container_statement
-  (container_name) @variable.other.member)
+; Scope bare keyword strings to their parent nodes to avoid
+; false matches inside identifiers (e.g. "as" in "ease-out")
+[
+  (as_clause "as")
+  (with_clause "with")
+  (visibility_clause "hide")
+  (visibility_clause "show")
+] @keyword
 
-(margin_at_keyword) @keyword
-(font_feature_value_keyword) @keyword
-
-(style_query "style" @function.builtin)
-(scroll_state_query "scroll-state" @function.builtin)
-(import_layer "layer" @function.builtin)
-(import_supports "supports" @function.builtin)
+[
+  (selector_query "selector")
+  (style_query "style")
+  (scroll_state_query "scroll-state")
+  (font_tech_query "font-tech")
+  (font_format_query "font-format")
+  (at_rule_query "at-rule")
+  (named_feature_query "named-feature")
+  (import_layer "layer")
+  (import_supports "supports")
+  (if_style_condition "style")
+  (if_media_condition "media")
+  (if_supports_condition "supports")
+  (if_sass_condition "sass")
+] @function.builtin
 
 (if_expression (function_name) @function.builtin)
-(if_style_condition "style" @function.builtin)
-(if_media_condition "media" @function.builtin)
-(if_supports_condition "supports" @function.builtin)
-(if_sass_condition "sass" @function.builtin)
 (if_else_condition) @keyword.control.conditional
 
 (style_condition
@@ -137,25 +175,26 @@
 "@return" @keyword.control.return
 
 [
-    "@else"
-    "@if"
+  "@else"
+  "@if"
 ] @keyword.control.conditional
+(else_if_clause "if" @keyword.control.conditional)
 
+; Scope loop keywords to their parent nodes
 [
   "@while"
   "@each"
   "@for"
-  "through"
-  "in"
-  "from"
-  "if"
 ] @keyword.repeat
+(for_statement "through" @keyword.repeat)
+(for_statement "from" @keyword.repeat)
+(each_statement "in" @keyword.repeat)
 
 [
-    "@forward"
-    "@import"
-    "@include"
-    "@use"
+  "@forward"
+  "@import"
+  "@include"
+  "@use"
 ] @keyword.control.import
 
 (string_value) @string
